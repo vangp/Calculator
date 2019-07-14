f77515079ebd688c78303e8201ac34a6ce5078cf
vangp <52622908+vangp@users.noreply.github.com>
2019-07-14 17:25:33 -0400

    Add files via upload

---
+++ Goals.py
@@ -0,0 +1,5 @@
+#GOALS:
+#Make calculator
+#Can do: addition, subtraction, multiplication, division.
+
+#Used https://codescracker.com/python/program/python-program-make-calculator.htm as reference material.
